07b98d0b5ed66fe27b27a4680a5431ee1121cda7
chkim <i__l@naver.com>
2025-03-26 23:06:45 +0900

    refactor: DB 스키마 업데이트

--- order-project/order/src/main/resources/schema.sql
+++ order-project/order/src/main/resources/schema.sql
@@ -5,15 +5,15 @@ create table `order`
     user_id                 varchar(255),
     status                  varchar(255),
     total_price             bigint,
-    order_created_date_time   datetime(6),
-    ordered_date_time   datetime(6),
+    order_created_date_time datetime(6),
+    ordered_date_time       datetime(6),
     road_addr               varchar(1000),
     jibun_addr              varchar(1000),
-    detail_addr            varchar(1000),
-    created_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
-    created_id       varchar(255),
-    modified_date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
-    modified_id      varchar(255),
+    detail_addr             varchar(1000),
+    created_date            TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+    created_id              varchar(255),
+    modified_date           TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
+    modified_id             varchar(255),
     primary key (id)
 ) engine=InnoDB;
 
@@ -22,17 +22,17 @@ alter table `order` auto_increment = 20000000;
 drop table if exists order_product;
 create table order_product
 (
-    id               bigint auto_increment,
-    order_id         bigint,
-    product_Id       varchar(255),
-    product_name     varchar(255),
-    quantity         int,
-    price            bigint,
-    total_price      bigint,
-    created_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
-    created_id       varchar(255),
-    modified_date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
-    modified_id      varchar(255),
+    id            bigint auto_increment,
+    order_id      bigint,
+    product_Id    bigint,
+    product_name  varchar(255),
+    quantity      bigint,
+    price         bigint,
+    total_price   bigint,
+    created_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+    created_id    varchar(255),
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
+    modified_id   varchar(255),
     primary key (id)
 ) engine=InnoDB;
 
--- product-project/product/src/main/resources/schema.sql
+++ product-project/product/src/main/resources/schema.sql
@@ -2,10 +2,9 @@ drop table if exists product;
 create table product
 (
     id               bigint auto_increment,
-    description      varchar(255),
-    image_url        varchar(255),
-    name             varchar(255),
-    price            varchar(255),
+    description      longtext,
+    name             varchar(1000),
+    price            bigint,
     current_quantity bigint,
     created_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
     created_id       varchar(255),
@@ -22,14 +21,14 @@ create table product_image
     id            BINARY(16) not null,
     product_id    bigint,
     file_id       binary(16),
-    file_name     varchar(255),
-    sequence      int,
-    origin_url    varchar(255),
-    thumbnail_url varchar(255),
-    created_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
-    created_id       varchar(255),
-    modified_date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
-    modified_id      varchar(255),
+    file_name     varchar(1000),
+    sequence      longint,
+    origin_url    varchar(1000),
+    thumbnail_url varchar(1000),
+    created_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+    created_id    varchar(255),
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
+    modified_id   varchar(255),
     primary key (id)
 ) engine=InnoDB;
 
